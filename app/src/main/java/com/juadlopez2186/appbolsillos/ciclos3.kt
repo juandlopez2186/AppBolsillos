7b7af985aa8b6cafd8320d816aea40ff515d84b2
S-117 <psyco2186@gmail.com>
2023-07-14 16:22:22 -0500

    proyecto kotlin

---
+++ app/src/main/java/com/juadlopez2186/appbolsillos/ciclos3.kt
@@ -0,0 +1,33 @@
+package com.juadlopez2186.appbolsillos
+
+class ciclos3 {
+}
+
+fun main() {
+    println("digite el inicio de numeros ")
+    var ini= readLine()!!.toInt()
+    println("digite el limite de numeros ")
+    var lim= readLine()!!.toInt()
+    var pr=0
+    var ipr=0
+    var pa=0
+    var per=0
+    for (i in 1..3) {
+        var n = i % 2
+        if (ini >=1) {
+            if (n == 0) {
+                pr = pr + 1
+                pa = pa + i
+            } else {
+                per = per + i
+                ipr = ipr + 1
+            }
+        }
+        else{
+            println("debe ser mayor a cero")
+            break
+        }
+    }
+    println("$pr son numeros pares y su sumatoria es $pa y $ipr son impares y su sumatoria es $per")
+
+}
---
+++ app/src/main/java/com/juadlopez2186/appbolsillos/in3.kt
@@ -0,0 +1,8 @@
+package com.juadlopez2186.appbolsillos
+
+class in3 {
+}
+
+fun main() {
+
+}
